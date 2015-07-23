056f033729d4f98e1bef74832588ccfe668593bf
Codemis <johnathan@jpulos.com>
2015-07-23 15:45:20 -0700

    Added some configuration parameters to make the module more testable.

--- tools/textgenerator/uw-grab-bibles.js
+++ tools/textgenerator/uw-grab-bibles.js
@@ -3,4 +3,13 @@
  * Read the README.md file at the root of this project for more details.
  */
 var uw = require('unfolding-word/uw-grab-available-texts');
+/**
+ * Set some of the settings before processing
+ *
+ */
+uw.catalogUrl = 'https://api.unfoldingword.org/uw/txt/2/catalog.json';
+uw.destinationFolder = 'input';
+/**
+ * Process the current Bibles
+ */
 uw.process();
