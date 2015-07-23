3539d5e778b4e0f59bc4aa8a651672100cf7ffec
Codemis <johnathan@jpulos.com>
2015-07-23 15:03:25 -0700

    Moved files around for easier testing.

---
+++ tools/textgenerator/uw-grab-bibles.js
@@ -0,0 +1,6 @@
+/**
+ * Grab the latest bibles from Unfolding Word, and prepare them for integrating in this app.
+ * Read the README.md file at the root of this project for more details.
+ */
+var uw = require('unfolding-word/uw-grab-available-texts');
+uw.process();
