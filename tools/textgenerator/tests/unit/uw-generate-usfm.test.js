3f3104be5dcf8d952acc8dae93220624b5a003f9
Codemis <johnathan@jpulos.com>
2015-07-30 10:55:43 -0700

    Generate now returns the correct About HTML data.

--- tools/textgenerator/tests/unit/uw-generate-usfm.test.js
+++ tools/textgenerator/tests/unit/uw-generate-usfm.test.js
@@ -7,10 +7,13 @@ var should = chai.should();
 var mockery = require('mockery');
 var sinon = require('sinon');
 var baseInfoJson = {"id":"uw_en_udb","abbr":"UDB","name":"Unlocked Dynamic Bible","nameEnglish":"","lang":"eng","langName":"English","langNameEnglish":"English","dir":"ltr","generator":"uw_usfm"};
+var fs = require('fs');
+var path = require('path');
 
 describe('uwGenerateUsfm', function() {
 
   var uw;
+  var testFilePath;
 
   before(function() {
     mockery.enable({
@@ -18,13 +21,14 @@ describe('uwGenerateUsfm', function() {
       warnOnUnregistered: false,
       useCleanCache: true
     });
+    testFilePath = path.join('tests', 'support', 'usfm_test_files');
     uw = require('unfolding-word/uw-generate-usfm');
   });
 
   describe("Function: generate()", function() {
 
     it("should return an object with the correct keys", function() {
-      var inputBasePath = 'tests/support/usfm_test_files/short_books';
+      var inputBasePath = path.join(testFilePath, 'short_books');
       var result = uw.generate(inputBasePath, baseInfoJson, false, function(){}, function() {});
       (result.hasOwnProperty('chapterData')).should.be.equal(true);
       (result.hasOwnProperty('indexData')).should.be.equal(true);
@@ -33,8 +37,30 @@ describe('uwGenerateUsfm', function() {
     });
     
     it("should return an array for each book with the correct object", function() {
-      var inputBasePath = 'tests/support/usfm_test_files/short_books';
+      var inputBasePath = path.join(testFilePath, 'short_books');
+      console.log(inputBasePath);
       var result = uw.generate(inputBasePath, baseInfoJson, false, function(){}, function() {});
+    });
+
+    describe("Return Data: aboutHtml", function() {
+
+      it("should return the about.html content if the file exists", function() {
+        var inputBasePath = path.join(testFilePath, 'short_books_with_about');
+        var aboutUsFilePath = path.join(inputBasePath, 'about.html');
+        var expected = fs.readFileSync(aboutUsFilePath, 'utf8');
+
+        var result = uw.generate(inputBasePath, baseInfoJson, false, function(){}, function() {});
+        expected.should.be.equal(result.aboutHtml);
+      });
+
+      it("should return an empty string if the file does not exist", function() {
+        var inputBasePath = path.join(testFilePath, 'short_books');
+        var expected = '';
+
+        var result = uw.generate(inputBasePath, baseInfoJson, false, function(){}, function() {});
+        expected.should.be.equal(result.aboutHtml);
+      });
+
     });
 
   });
