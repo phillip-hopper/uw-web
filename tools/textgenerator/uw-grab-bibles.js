b6f7d439b58f7632338615580fc676cef0045e09
Codemis <johnathan@jpulos.com>
2015-11-03 09:34:01 -0800

    Bug Fix: Receiving ENOENT error on script when run
    Changed the pathing to an absolute pathing to see if it remedies the
    issue.

--- tools/textgenerator/uw-grab-bibles.js
+++ tools/textgenerator/uw-grab-bibles.js
@@ -3,12 +3,13 @@
  * Read the README.md file at the root of this project for more details.
  */
 var uw = require('unfolding-word/uw-grab-available-texts');
+var path = require('path');
 /**
  * Set some of the settings before processing
  *
  */
 uw.catalogUrl = 'https://api.unfoldingword.org/uw/txt/2/catalog.json';
-uw.destinationFolder = 'input';
+uw.destinationFolder = path.join(__dirname, 'input');
 /**
  * Process the current Bibles
  */
